534fa4290dd9a77a414425f583b35d93bd559105
ooocha <optionboy@naver.com>
2023-02-08 20:54:34 +0900

    feat : init

---
+++ tutorial/tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  tutorialApp.swift
+//  tutorial
+//
+//  Created by Cha on 2023/02/08.
+//
+
+import SwiftUI
+
+@main
+struct tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
